eb80ae596e8426ddf65410191e79acc9e0e0d7de
Colin Mitchell <muffinista@gmail.com>
2021-08-13 12:15:21 -0400

    Disable for the moment

--- namey_app.rb
+++ namey_app.rb
@@ -35,35 +35,35 @@ get '/' do
   erb :index
 end
   
-get '/name.?:format?' do
-  opts = {
-    :frequency => :common
-  }.merge(params.to_h.symbolize_keys!)
+#get '/name.?:format?' do
+  # opts = {
+  #   :frequency => :common
+  # }.merge(params.to_h.symbolize_keys!)
 
-  opts[:with_surname] = (params[:with_surname] == true || params[:with_surname] == "true")
+  # opts[:with_surname] = (params[:with_surname] == true || params[:with_surname] == "true")
 
-  [:type, :frequency].each do |key|
-    opts[key] = opts[key].to_sym if opts.has_key?(key)
-  end
+  # [:type, :frequency].each do |key|
+  #   opts[key] = opts[key].to_sym if opts.has_key?(key)
+  # end
 
-  opts.delete(:type) if ! [:male, :female, :surname].include?(opts[:type])
+  # opts.delete(:type) if ! [:male, :female, :surname].include?(opts[:type])
   
-  count = (params.delete(:count) || 1).to_i
-  count = 10 if count > 10
+  # count = (params.delete(:count) || 1).to_i
+  # count = 10 if count > 10
 
-  names = 1.upto(count).collect do
-    $generator.generate(opts)
-  end.compact
+  # names = 1.upto(count).collect do
+  #   $generator.generate(opts)
+  # end.compact
 
-  if params[:format] == "json"
-    content_type :json, 'charset' => 'utf-8'
-    tmp = JSON.generate names
-    if params[:callback]
-      "#{params[:callback]}(#{tmp});"
-    else
-      tmp
-    end
-  else
-    ["<ul>", names.collect { |n| "<li>#{n}</li>" }.join(" "), "</ul>"].join("")
-  end
-end 
+  # if params[:format] == "json"
+  #   content_type :json, 'charset' => 'utf-8'
+  #   tmp = JSON.generate names
+  #   if params[:callback]
+  #     "#{params[:callback]}(#{tmp});"
+  #   else
+  #     tmp
+  #   end
+  # else
+  #   ["<ul>", names.collect { |n| "<li>#{n}</li>" }.join(" "), "</ul>"].join("")
+  # end
+#end 
